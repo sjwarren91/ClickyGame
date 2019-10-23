eeccf85dfc1eff2d9fea7b0058e6313fc22bb279
Sam Warren <sjwarren91@gmail.com>
2019-10-23 18:21:37 +1100

    navbar component

---
+++ src/components/NavBar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./NavBar.css";
+
+function NavBar() {
+    return <div className="header">
+        <div className="Title">
+            Click and Morty
+        </div>
+    </div>
+}
+
+export default NavBar
