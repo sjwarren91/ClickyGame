b42aa611d85d9351b9ca66c172ebe2a3e2bbfb68
Sam Warren <sjwarren91@gmail.com>
2019-10-26 12:19:04 +1100

    Updates

---
+++ precache-manifest.6b8661a28848ff88b157f4bbf896c055.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "486e5e96c1ab71ae78da3b0a2e417aa9",
+    "url": "/ClickandMorty/index.html"
+  },
+  {
+    "revision": "8abcadf09423a0348b0f",
+    "url": "/ClickandMorty/static/css/main.1e10e316.chunk.css"
+  },
+  {
+    "revision": "7fab496e1fc045cd0096",
+    "url": "/ClickandMorty/static/js/2.c9bf7561.chunk.js"
+  },
+  {
+    "revision": "8abcadf09423a0348b0f",
+    "url": "/ClickandMorty/static/js/main.afff95a6.chunk.js"
+  },
+  {
+    "revision": "cb2b7c51992c6ba74ae3",
+    "url": "/ClickandMorty/static/js/runtime-main.11fa51e7.js"
+  }
+]);
